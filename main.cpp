6b855c8a9bad0401b9b48de019259b50d5b740e5
minanrohman <113458650+minanrohman@users.noreply.github.com>
2022-09-13 23:22:25 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "NAMA: MOH MIANUR ROHMAN" << endl;
+    cout << "ALAMAT: PATI JAWA TENGAH" << endl;
+    cout << "NIM: A11.2022.14597" << endl;
+    return 0;
+}
